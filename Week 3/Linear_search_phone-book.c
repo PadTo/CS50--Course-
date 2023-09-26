6d5343685197b2b2616d1db86f6758bc7697d5fe
padegimas <tpadegimas4@gmail.com>
2023-09-26 12:21:27 +0200

    Adding a data structure

--- Week 3/Linear_search_phone-book.c
+++ Week 3/Linear_search_phone-book.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct Data_structure
+{
+    char name[20];
+    char number[20];
+} person;
+
 /*Searching a phone book using linear search*/
 int main(void)
 {
-    const int n = 3;
-    char names[][20] = {"Carter", "David", "Tom"};
-    char numbers[][20] = {"+370666", "+370777", "+370888"};
+    const int N = 2;
+    person people[N];
+    strcpy(people[0].name, "Carter");
+    strcpy(people[0].number, "+37066");
+
+    strcpy(people[1].name, "Jake");
+    strcpy(people[1].number, "+37077");
 
     char search_name[20];
     printf("Whose phone number are you looking for?\n");
     scanf("%s", &search_name);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
     {
-        if (strcmp(names[i], search_name) == 0)
+        if (strcmp(people[i].name, search_name) == 0)
         {
-            printf("%s's number is %s", search_name, numbers[i]);
+            printf("%s's number is %s", search_name, people[i].number);
             return 0;
         }
     }
