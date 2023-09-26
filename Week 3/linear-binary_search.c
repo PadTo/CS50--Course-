a84cafa0fb1407f9287bf739fe97c82102e70357
padegimas <tpadegimas4@gmail.com>
2023-09-26 10:20:30 +0200

    Creating a linear search algo

---
+++ Week 3/linear-binary_search.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int doors[] = {1, 20, 30, 50, 100, 170};
+    int n;
+
+    printf("What number do you want to find?\n");
+    scanf("%i", &n);
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (doors[i] == n)
+        {
+            printf("Found!\n");
+            return 0;
+        }
+    }
+    printf("Not Found...\n");
+    return 1;
+}
